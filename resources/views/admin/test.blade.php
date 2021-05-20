f2df30adbed95ba4ddb358cd9eeb1ac9a2544faa
Nitesh Vishwakarma <45270353+NiteshVishwakarma896@users.noreply.github.com>
2021-05-20 22:42:31 +0530

    Added Layouts and Home Page and CSS AND JS FOLDERS

---
+++ resources/views/admin/test.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.index')
+@section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Test page
+        
+      </h1>
+    
+    </section>
+
+   
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.home');
 });
+Route::get('/test', function () {
+    return view('admin.test');
+});
+
